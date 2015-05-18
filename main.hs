9648fb741b5a2ba0a077cc5916b997657c17e9b4
Marc Espinosa <marespinosa@ebay.com>
2015-05-19 00:40:53 +0200

    Adding missing types

--- main.hs
+++ main.hs
@@ -1,24 +1,30 @@
 -- Problem 1
+myLast :: [a] -> a
 myLast (x:[]) = x
 myLast (x:xs) = myLast xs
 
 -- Problem 2
+myButLast :: [a] -> a
 myButLast (x:y:[]) = x
 myButLast (_:xs) = myButLast xs
 
 -- Problem 3
+elementAt :: [a] -> Int -> a
 elementAt (x:_) 1 = x
 elementAt (_:xs) n = elementAt xs (n-1)
 
 -- Problem 4
+myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
 
 -- Problem 5
+myReverse :: [a] ->[a]
 myReverse (x:[]) = [x]
 myReverse (x:xs) = myReverse xs ++ [x]
 
 -- Problem 6
+isPalindrome :: Eq a => [a] -> Bool
 isPalindrome [] = True
 isPalindrome (x:[]) = True
 isPalindrome (xs) = ((head xs) == (last xs)) && (isPalindrome (init (tail xs)))
@@ -31,11 +37,14 @@ flatten (List (x:xs)) = flatten x ++ flatten (List xs)
 flatten (List []) = []
 
 -- Problem 8
+whatever :: Eq a => [a] -> a -> [a]
 whatever [] x = [x]
 whatever xs x = if last xs == x then xs else xs ++ [x]
+compress :: Eq a => [a] -> [a]
 compress (x) = foldl (whatever) [] x
 
 -- Problem 9
+pack :: Eq a => [a] -> [[a]]
 pack [] = []
 pack (x:xs) = [[x] ++ takeWhile (== x) xs] ++ pack (dropWhile (== x) xs)
 
@@ -56,9 +65,11 @@ decodeModified ((Multiple 0 x):xs) = []
 decodeModified ((Multiple n x):xs) = decodeModified [Single x] ++ decodeModified [Multiple (n-1) x] ++ decodeModified xs
 
 -- Problem 14
+dupli :: Eq a => [a] -> [a]
 dupli [] = []
 dupli (x:xs) = [x] ++ [x] ++ dupli xs
 
 -- Problem 15
+repli :: Eq a => [a] -> Int -> [a]
 repli [] n = []
 repli (x:xs) n = [x | i <- [1..n]] ++ repli xs n
